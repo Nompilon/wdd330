c6700a827e7136bafeec3e7ecb18358b6dce036e
Nompilon <ngwenyanompilo@gmail.com>
2026-02-15 02:20:18 +0200

    ...

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        meals: resolve(__dirname, "src/meals/index.html"),
+        meals: resolve(__dirname, "src/recipes/index.html"),
         about: resolve(__dirname, "src/about/index.html"),
         track: resolve(__dirname, "src/track/index.html"),
       },
